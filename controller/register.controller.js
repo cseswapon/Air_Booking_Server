a37f3b15afcc9a89c8a26ca571a8c9771c152fad
cseswapon <cseswapon@gmail.com>
2023-03-20 23:09:37 +0600

    [ADD] - added with a duplicated email trak

--- controller/register.controller.js
+++ controller/register.controller.js
@@ -1,19 +1,29 @@
 const bcrypt = require("bcrypt");
+const { findUser } = require("../services/login.service");
 const { registerService } = require("../services/register.service");
 
 module.exports.userCreate = async (req, res) => {
   try {
-    const { password } = req.body;
-    const newBody = {
-      ...req.body,
-      password: await bcrypt.hash(password, 10),
-    };
-    const data = await registerService(newBody);
-    res.status(200).send({
-      status: "Success",
-      message: "Successfully created the brand",
-      data,
-    });
+    const { email, password } = req.body;
+    const user = await findUser(email);
+    if (!user) {
+      const newBody = {
+        ...req.body,
+        password: await bcrypt.hash(password, 10),
+      };
+      const data = await registerService(newBody);
+      res.status(200).send({
+        status: "Success",
+        message: "Successfully created the brand",
+        data,
+      });
+    } else {
+      res
+        .status(406)
+        .send({
+          message: `${email} is duplicated please try another unique email`,
+        });
+    }
   } catch (error) {
     res.status(404).send({ message: error.message });
   }
