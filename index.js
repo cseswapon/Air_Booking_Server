b89fee34c0cfdea642ebea931389bda56fc5595e
cseswapon <cseswapon@gmail.com>
2023-05-15 00:02:02 +0600

    [MOD] - simple modifiy with a backed server

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const signupRoute = require('./routes/register.route');
+const signupRoute = require("./routes/register.route");
+const { checkLogin } = require("./middlewares/checkLogion");
 // middleware
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/user", signupRoute);
 
-
 // public route test
 app.get("/", (req, res) => {
   res.send({ message: "Airt Ticket API Side Running" });
+});
+
+// this route is ver secure 
+app.get("/newService", checkLogin, async (req, res) => {
+  res.send({ message: "OK" });
 });
 
 // not found route
