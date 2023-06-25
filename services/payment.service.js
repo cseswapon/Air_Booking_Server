dbeb6f4fefa9374bd98d021f8ba3ad2d3dbacbe8
cseswapon <cseswapon@gmail.com>
2023-06-26 01:09:07 +0600

    [ADD] - added to payment route

--- services/payment.service.js
+++ services/payment.service.js
@@ -1,7 +1,7 @@
 const Payment = require("../modules/Payment.module");
 
 module.exports.getServicePayment = () => {
-    return Payment.find({}).populate("user","_id, personalName,");
+    return Payment.find({}).populate("user", "-_id email companyName");
 }
 
 module.exports.postServicePayment = (body) => {
